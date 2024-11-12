e1fe567824aa61b74863d36ffe9d965785ccfded
Bdiplus <bdiplus@Bdipluss-MacBook-Air.local>
2024-11-12 13:24:22 +0530

    Login page Completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Dashboard from '../views/Dashboard.vue'
-import DefectDensity from '../views/DefectDensity.vue'
-import CodeInsight from '../views/CodeInsight.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Dashboard from "../views/Dashboard.vue";
+import DefectDensity from "../views/DefectDensity.vue";
+import CodeInsight from "../views/CodeInsight.vue";
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes: [
-		{
-			path: '/',
-			component: Home
-		},
-		{
-			path: '/dashboard',
-			component: Dashboard
-		},
-		{
-			path: '/defect-density',
-			component: DefectDensity
-		},
-		{
-			path: '/code-insight',
-			component: CodeInsight
-		},
-	],
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/home",
+      component: Home,
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+    },
+    {
+      path: "/defect-density",
+      component: DefectDensity,
+    },
+    {
+      path: "/code-insight",
+      component: CodeInsight,
+    },
+  ],
+});
 
-export default router
+export default router;
